3badaac9fa14b68614627716f67f69117d36ea5b
Evgeniy Gritsienko <evgeniigricienko@mail.ru>
2022-09-07 16:54:27 +0400

    Formed intro skewed block, injected  slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.skewed__list').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        slidesToScroll: 1,
+        variableWidth: true,
+      });
+
+});
